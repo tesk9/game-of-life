f4b2d5f383024b128baed403c6b26082eec646a2
Tessa Kelly <tessakkelly@gmail.com>
2015-10-28 00:00:50 -0700

    Adds reproduce logic

--- Update.elm
+++ Update.elm
@@ -13,7 +13,31 @@ update action model =
     SetLifeBeginning beginningCoords ->
       ({ model | coordsLife <- (beginningCoords :: model.coordsLife) }, Effects.none)
     Reproduce ->
-      (model, Effects.none)
+      let
+        getLivingNeighbors : (Int, Int) -> List (Int, Int)
+        getLivingNeighbors (x,y) =
+          if (List.member (x + 1, y + 1) model.coordsLife) then
+            [ (x + 1, y)
+            , (x - 1, y)
+            ]
+          else if (List.member (x, y + 2) model.coordsLife) then
+            [ (x, y + 1) ]
+          else if (List.member (x + 2, y) model.coordsLife) then
+            [ (x + 1, y) ]
+          else
+            []
+
+        limitToGridSize : (Int, Int) -> Bool
+        limitToGridSize (x,y) =
+            x > 0 && x <= model.numberOfRows && y > 0 && y <= model.numberOfCols
+
+        newCoordsLife : List (Int, Int)
+        newCoordsLife =
+          model.coordsLife
+            |> List.concatMap getLivingNeighbors
+            |> List.filter limitToGridSize
+      in
+        ({ model | coordsLife <- newCoordsLife }, Effects.none)
 
 type Action
   = NoOp
